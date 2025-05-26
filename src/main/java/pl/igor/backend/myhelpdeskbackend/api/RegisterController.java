edab6799ab9f024c13a5923e7d80a6b20a9bf311
IgorStrzezysz1 <93446017+IgorStrzezysz1@users.noreply.github.com>
2025-05-27 00:06:41 +0200

    Nowe poprawki kodu

--- src/main/java/pl/igor/backend/myhelpdeskbackend/api/RegisterController.java
+++ src/main/java/pl/igor/backend/myhelpdeskbackend/api/RegisterController.java
@@ -2,30 +2,22 @@ package pl.igor.backend.myhelpdeskbackend.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.igor.backend.myhelpdeskbackend.model.UserEntity;
 import pl.igor.backend.myhelpdeskbackend.service.RegisterService;
 
 @RestController
 @RequestMapping("/api/v1")
 public class RegisterController {
-    private RegisterService registerService;
+
+    private final RegisterService registerService;
 
     public RegisterController(RegisterService registerService) {
         this.registerService = registerService;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(
-            @RequestHeader("X-Auth-Email") String email,
-            @RequestHeader("X-Auth-Name") String firstName,
-            @RequestHeader("X-Auth-Surname") String lastName,
-            @RequestHeader("X-Auth-Password") String password) {
-
-        UserEntity userEntity = new UserEntity(email, firstName, lastName, password);
+    public ResponseEntity<String> registerUser(@RequestBody UserEntity userEntity) {
         boolean success = registerService.registerUser(userEntity);
         if (success) {
             return ResponseEntity.ok("Rejestracja zakończona sukcesem");
