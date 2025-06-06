a740fd6f65ad643ed195048b262a151ef5e7505b
IgorStrzezysz1 <93446017+IgorStrzezysz1@users.noreply.github.com>
2025-06-06 11:43:28 +0200

    Koniec bliski

--- src/main/java/pl/igor/backend/myhelpdeskbackend/api/dto/LoginController.java
+++ src/main/java/pl/igor/backend/myhelpdeskbackend/api/dto/LoginController.java
@@ -3,26 +3,23 @@ package pl.igor.backend.myhelpdeskbackend.api.dto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.igor.backend.myhelpdeskbackend.service.LoginService;
 import pl.igor.backend.myhelpdeskbackend.service.RegisterService;
 
 @RestController
 @RequestMapping("/api/v1")
 public class LoginController {
 
-    private final RegisterService registerService;
+    private final LoginService loginService;
 
-    public LoginController(RegisterService registerService) {
-        this.registerService = registerService;
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestHeader("X-Auth-Email") String email,
+    public ResponseEntity<LoginDto> loginUser(@RequestHeader("X-Auth-Email") String email,
                                             @RequestHeader("X-Auth-Password") String password) {
-        boolean isValid = registerService.validateUser(email, password);
-        if (isValid) {
-            return ResponseEntity.ok("Zalogowano pomyślnie");
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Nieprawidłowe dane logowania");
-        }
+        return ResponseEntity.ok(loginService.loginUser(email, password));
+
     }
 }
